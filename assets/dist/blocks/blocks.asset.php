f5e55ae74e4c93dc84b32201b98b9b5b57da5541
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-08-18 00:05:50 +0200

    Recompile GB scripts

--- assets/dist/blocks/blocks.asset.php
+++ assets/dist/blocks/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '07169027d452c99bdb8e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ec3455b9ef5cddc95e6d');
