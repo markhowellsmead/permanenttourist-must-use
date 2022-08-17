d0428bc6360fa7982d61adacad3f705ee79bc621
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-08-17 23:49:54 +0200

    Revis PrimaryMedia block output

--- src/Blocks/PrimaryMedia/Block.php
+++ src/Blocks/PrimaryMedia/Block.php
@@ -20,6 +20,7 @@ class Block
 
 	public function renderBlock(array $attributes, string $content, WP_Block $block)
 	{
+		$html = '';
 		$video_ref = get_field('video_ref', get_the_ID());
 		$align = $attributes['align'] ?? '';
 		$classNameBase = wp_get_block_default_classname($block->name);
@@ -28,28 +29,45 @@ class Block
 			$align = "align{$align}";
 		}
 
-		if (!empty($video_ref)) {
+		$video = wp_oembed_get($video_ref);
 
-			$video = wp_oembed_get($video_ref);
+		if (!empty($video)) {
 
-			if (!empty($video)) {
-
-				ob_start();
-				// dump(get_object_vars($block));
+			ob_start();
 ?>
-				<div <?php echo get_block_wrapper_attributes(['class' => "with--videoplayer {$align}"]); ?>>
-					<div class="<?php echo $classNameBase; ?>__videoplayer">
-						<?php echo $video; ?>
-					</div>
+			<div <?php echo get_block_wrapper_attributes(['class' => "with--videoplayer {$align}"]); ?>>
+				<div class="<?php echo $classNameBase; ?>__videoplayer">
+					<?php echo $video; ?>
 				</div>
+			</div>
+
+		<?php
+			$html = ob_get_contents();
+			ob_end_clean();
+		} elseif (has_post_thumbnail(get_the_ID())) {
+			ob_start();
+		?>
+			<div <?php echo get_block_wrapper_attributes(['class' => "with--post-thumbnail {$align}"]); ?>>
+				<figure class="<?php echo $classNameBase; ?>__figure">
+					<?php echo wp_get_attachment_image(get_post_thumbnail_id(get_the_ID()), 'full', false, ['class' => "{$classNameBase}__image"]); ?>
+				</figure>
+			</div>
+		<?php
+			$html = ob_get_contents();
+			ob_end_clean();
+		} elseif (pt_must_use_get_instance()->Package->Gutenberg->isContextEdit()) {
+			ob_start();
+		?>
+			<div <?php echo get_block_wrapper_attributes(); ?>>
+				<p>BLOCK PLACEHOLDER</p>
+			</div>
 
 <?php
-
-			}
+			$html = ob_get_contents();
+			ob_end_clean();
 		}
 
-		$html = ob_get_contents();
-		ob_end_clean();
+
 
 		return $html;
 	}
