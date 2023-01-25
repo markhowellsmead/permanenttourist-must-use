80e84b6ec43d1303ae7a6963eac2f405798db0b9
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-01-25 23:42:13 +0100

    Webcam image aspect ratio

--- assets/dist/blocks/blocks.asset.php
+++ assets/dist/blocks/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '057a839d470d70f82703');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c160fbcbdfde0821760e');
