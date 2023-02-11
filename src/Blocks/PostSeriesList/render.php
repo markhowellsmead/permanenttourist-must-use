a24c6221815bba68f61f2831ed8e88588bc879f0
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-02-11 19:21:26 +0100

    More work on the series list view

--- src/Blocks/PostSeriesList/render.php
+++ src/Blocks/PostSeriesList/render.php
@@ -30,24 +30,71 @@ foreach ($terms as $term) {
 		continue;
 	}
 
+	$permalink = get_term_link($term);
+
 	$post_date = get_the_date('YmdHis', $posts[0]->ID);
 
-	$image = wp_get_attachment_image(get_term_meta($term->term_id, 'thumbnail', true), 'thumbnail', false, ['class' => "{$classNameBase}__image"]);
+	$image = wp_get_attachment_image(get_term_meta($term->term_id, 'thumbnail', true), 'medium', false, ['class' => "{$classNameBase}__image"]);
 
 	if (!empty($image)) {
 		$image = sprintf(
-			'<figure class="%1$s__figure">%2$s</figure>',
+			'<figure class="%1$s__figure"><a href="%2$s">%3$s</a></figure>',
 			$classNameBase,
+			$permalink,
 			$image
 		);
 	}
 
-	$series["ts{$post_date}"] =
-		sprintf('<div>%4$s <a href="%1$s">%2$s</a> (Most recent addition in %3$s)</div>', get_term_link($term), esc_html($term->name), get_the_date(get_option('date_format'), $posts[0]->ID), $image);
+	$description = '';
+
+	if (!empty($term->description)) {
+		$description = sprintf('<div class="%1$s__excerpt-content">%2$s</div>', $classNameBase, $term->description);
+	}
+
+	ob_start();
+?>
+	<li class="<?php echo $classNameBase; ?>__entry">
+		<div class="is-layout-constrained ___wp-container-11 wp-block-group">
+			<?php if (!empty($image)) { ?>
+				<div class="wp-block-sht-primary-media">
+					<?php echo $image; ?>
+				</div>
+			<?php } else { ?>
+				<div class="wp-block-sht-primary-media wp-block-sht-primary-media--empty"></div>
+			<?php } ?>
+
+			<div class="is-layout-constrained ___wp-container-10 wp-block-group <?php echo $classNameBase; ?>__content">
+				<h2 class="wp-block-post-title has-large-font-size"><a href="<?php echo $permalink; ?>" target="_self"><?php echo esc_html($term->name); ?></a></h2>
+				<div class="is-layout-flow ___wp-container-9 wp-block-group post-meta has-small-font-size">
+					<div class="wp-block-post-date">Most recent addition: <time><?php echo get_the_date(get_option('date_format'), $posts[0]->ID); ?></time></div>
+				</div>
+
+				<?php if (!empty($description)) { ?>
+					<div class="<?php echo $classNameBase; ?>__excerpt has-smaller-font-size h-stack">
+						<?php echo $description; ?>
+						<p class="wp-block-post-excerpt__more-text"><a class="wp-block-post-excerpt__more-link" href="<?php echo $permalink; ?>">Read more</a></p>
+					</div>
+				<?php } ?>
+			</div>
+		</div>
+	</li>
+<?php
+
+	//printf('<div>%4$s %5$s <a href="%1$s">%2$s</a> (Most recent addition in %3$s)</div>', $permalink, esc_html($term->name), get_the_date(get_option('date_format'), $posts[0]->ID), $image, $description);
+	$html = ob_get_contents();
+	ob_end_clean();
+	$series["ts{$post_date}"] = $html;
 }
 
 krsort($series);
 
-foreach ($series as $series_entry) {
-	echo $series_entry;
-}
+?>
+<div class="<?php echo $classNameBase; ?> is-layout-flow  alignwide">
+	<ul class="is-layout-flow is-flex-container columns-3 <?php echo $classNameBase; ?>__entries">
+		<?php
+		foreach ($series as $series_entry) {
+			echo $series_entry;
+		}
+		?>
+	</ul>
+</div>
