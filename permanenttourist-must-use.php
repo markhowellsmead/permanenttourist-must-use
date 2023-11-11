42661815b22f1a72fbaf779dd548f616031fa688
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-11-11 17:58:50 +0100

    Photo post type should not be hierarchical

--- permanenttourist-must-use.php
+++ permanenttourist-must-use.php
@@ -4,7 +4,7 @@ Plugin Name: Permanent Tourist Must Use
 Plugin URI: #
 Description: This WordPress plugin contains all of the blocks and portable functionality for permanenttourist.ch.
 Author: Mark Howells-Mead
-Version: 0.18.3
+Version: 0.18.4
 Author URI: https://www.permanenttourist.ch/
 Text Domain: pt-must-use
 Domain Path: languages
