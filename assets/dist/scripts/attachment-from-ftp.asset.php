83cbe1c2a948166b105b6c9f26859dd90ac0681a
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-11-12 19:02:47 +0100

    Recompiled

--- assets/dist/scripts/attachment-from-ftp.asset.php
+++ assets/dist/scripts/attachment-from-ftp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '1792b3913bc74afc18eb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => 'c393089516b5c49a5d21');
