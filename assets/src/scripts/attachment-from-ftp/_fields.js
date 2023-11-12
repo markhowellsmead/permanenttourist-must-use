06d01f607cf0b7a47f7b709e98524e0b83f164c9
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-11-12 19:00:31 +0100

    Add editing / non-editing state

--- assets/src/scripts/attachment-from-ftp/_fields.js
+++ assets/src/scripts/attachment-from-ftp/_fields.js
@@ -7,6 +7,7 @@ export const TitleField = ({ classNameBase, post }) => {
     const [fieldValue, setFieldValue] = useState(title.rendered);
     const [success, setSuccess] = useState(false);
     const [updating, setUpdating] = useState(false);
+    const [editing, setEditing] = useState(false);
 
     const updateTitle = () => {
         if (!fieldValue.length) {
@@ -49,20 +50,35 @@ export const TitleField = ({ classNameBase, post }) => {
 
     return (
         <div className={`${classNameBase}__fieldwrap ${classNameBase}__fieldwrap--title`}>
-            <RichText
-                className={`${classNameBase}__title ${classNameBase}__field ${classNameBase}__field--title ${empty_class}`}
-                value={fieldValue}
-                onChange={value => {
-                    setFieldValue(value);
-                }}
-            />
-            <button
-                disabled={!fieldValue.length || updating}
-                className='button button-primary'
-                onClick={updateTitle}
-            >
-                Update
-            </button>
+            {!editing && (
+                <h2
+                    className={`${classNameBase}__title`}
+                    dangerouslySetInnerHTML={{ __html: fieldValue }}
+                    onClick={() => setEditing(true)}
+                />
+            )}
+            {editing && (
+                <>
+                    <RichText
+                        tagName='h2'
+                        className={`${classNameBase}__title ${classNameBase}__field ${classNameBase}__field--title ${empty_class}`}
+                        value={fieldValue}
+                        onChange={value => {
+                            setFieldValue(value);
+                        }}
+                    />
+                    <button
+                        disabled={!fieldValue.length || updating}
+                        className='button button-primary'
+                        onClick={updateTitle}
+                    >
+                        Update
+                    </button>
+                    <button className='button button-secondary' onClick={() => setEditing(false)}>
+                        Cancel
+                    </button>
+                </>
+            )}
             {success && (
                 <div
                     className={`${classNameBase}__fieldwrap-message ${classNameBase}__fieldwrap-message--success`}
