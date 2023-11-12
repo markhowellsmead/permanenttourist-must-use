76d1e86242632ff71de7de6d735536cd7344a5e5
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-11-12 12:21:12 +0100

    Add Attachment from FTP process

--- src/Plugin.php
+++ src/Plugin.php
@@ -98,6 +98,7 @@ class Plugin
 				Package\Archives::class,
 				Package\Assets::class,
 				Package\AttachmentGeoMetadata::class,
+				Package\AttachmentFromFTP::class,
 				// Package\CLI::class, // Only comment in when in use
 				Package\Flickr::class,
 				Package\Gutenberg::class,
