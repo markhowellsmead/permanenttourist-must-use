67d1c0a450afeffb4a051ccd9342eb22b3b4d7a1
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-03-14 16:22:32 +0100

    Allow image resolution to be selected

--- src/Blocks/PrimaryMedia/dist/scripts/editor.asset.php
+++ src/Blocks/PrimaryMedia/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ef9e66041c7f6809810a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2ed24a14bffd908234cb');
