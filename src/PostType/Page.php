dcdfd351bede9d5ca3841399fbdd3ea7af24d34c
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-11-23 11:07:11 +0100

    Add page control meta

--- src/PostType/Page.php
+++ src/PostType/Page.php
@@ -12,10 +12,21 @@ class Page
 	public function run()
 	{
 		add_filter('init', [$this, 'allowExcerpt']);
+		add_filter('init', [$this, 'registerMeta']);
 	}
 
 	public function allowExcerpt()
 	{
 		add_post_type_support('page', 'excerpt');
+	}
+
+	public function registerMeta()
+	{
+		register_post_meta('page', 'content_behind_masthead', [
+			'type'         => 'boolean',
+			'single'       => true,
+			'show_in_rest' => true,
+			'default'      => false,
+		]);
 	}
 }
