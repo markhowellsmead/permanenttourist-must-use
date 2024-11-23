ca8898b51154ecf3f8231126a01c6d11b8555870
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-11-23 11:07:44 +0100

    Recompiled

--- assets/dist/blocks/blocks.asset.php
+++ assets/dist/blocks/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'bc539ba4230da43ff02d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '5fc072e9cc42a793f34a');
