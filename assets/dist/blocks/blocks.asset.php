6214a307d21de3151371f3fbcb333170e4c5b4ac
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-11-23 11:08:17 +0100

    Compatibility fixes

--- assets/dist/blocks/blocks.asset.php
+++ assets/dist/blocks/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '5fc072e9cc42a793f34a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '2b404e23979718396eee');
