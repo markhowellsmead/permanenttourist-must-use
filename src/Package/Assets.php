4f0b587dc54a974cb11ff70a34f0fbe037ffdffa
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-11-23 11:07:26 +0100

    Revise asset keys

--- src/Package/Assets.php
+++ src/Package/Assets.php
@@ -21,9 +21,8 @@ class Assets
 		$dir_path = plugin_dir_path(pt_must_use_get_instance()->file);
 		$dir_url = plugin_dir_url(pt_must_use_get_instance()->file);
 
-		wp_enqueue_style('permanenttourist-must-use-style', $dir_url . 'assets/dist/styles/ui' . ($min ? '.min' : '') . '.css', [], filemtime($dir_path . 'assets/dist/styles/ui' . ($min ? '.min' : '') . '.css'));
-
-		wp_enqueue_script('permanenttourist-must-use-video', $dir_url . 'assets/dist/scripts/video.js', ['jquery'], filemtime($dir_path . 'assets/dist/scripts/video.js'), true);
+		wp_enqueue_style('pt-must-use-style', $dir_url . 'assets/dist/styles/ui' . ($min ? '.min' : '') . '.css', [], filemtime($dir_path . 'assets/dist/styles/ui' . ($min ? '.min' : '') . '.css'));
+		wp_enqueue_script('pt-must-use-video', $dir_url . 'assets/dist/scripts/video.js', ['jquery'], filemtime($dir_path . 'assets/dist/scripts/video.js'), true);
 	}
 
 	public function registerAdminAssets()
