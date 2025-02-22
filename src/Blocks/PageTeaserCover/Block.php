c6a3d37334b70088b9aa0f012262b6e1cdcb4e53
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-02-22 19:01:49 +0100

    Frontend rendering

--- src/Blocks/PageTeaserCover/Block.php
+++ src/Blocks/PageTeaserCover/Block.php
@@ -13,4 +13,84 @@ class Block
 	{
 		register_block_type(__DIR__);
 	}
+
+	public static function contentStylesCalcString(array $style): string
+	{
+		$contentStyles = [];
+
+		if ($style['spacing']['padding'] ?? false) {
+			if (!empty($style['spacing']['padding']['top'] ?? '')) {
+				if (strpos($style['spacing']['padding']['top'], 'var:preset') !== false) {
+					$parts = explode('|', $style['spacing']['padding']['top']);
+					$size = $parts[2] ?? '';
+					$contentStyles['padding-top'] = "var(--wp--preset--spacing--{$size})";
+				} else {
+					$contentStyles['padding-top'] = $style['spacing']['padding']['top'];
+				}
+			}
+
+			if (!empty($style['spacing']['padding']['right'] ?? '')) {
+				if (strpos($style['spacing']['padding']['right'], 'var:preset') !== false) {
+					$parts = explode('|', $style['spacing']['padding']['right']);
+					$size = $parts[2] ?? '';
+					$contentStyles['padding-right'] = "var(--wp--preset--spacing--{$size})";
+				} else {
+					$contentStyles['padding-right'] = $style['spacing']['padding']['right'];
+				}
+			}
+
+			if (!empty($style['spacing']['padding']['bottom'] ?? '')) {
+				if (strpos($style['spacing']['padding']['bottom'], 'var:preset') !== false) {
+					$parts = explode('|', $style['spacing']['padding']['bottom']);
+					$size = $parts[2] ?? '';
+					$contentStyles['padding-bottom'] = "var(--wp--preset--spacing--{$size})";
+				} else {
+					$contentStyles['padding-bottom'] = $style['spacing']['padding']['bottom'];
+				}
+			}
+
+			if (!empty($style['spacing']['padding']['left'] ?? '')) {
+				if (strpos($style['spacing']['padding']['left'], 'var:preset') !== false) {
+					$parts = explode('|', $style['spacing']['padding']['left']);
+					$size = $parts[2] ?? '';
+					$contentStyles['padding-left'] = "var(--wp--preset--spacing--{$size})";
+				} else {
+					$contentStyles['padding-left'] = $style['spacing']['padding']['left'];
+				}
+			}
+		}
+
+		if (empty($contentStyles)) {
+			return '';
+		}
+
+		return implode('; ', array_map(function ($value, $property) {
+			return "{$property}: {$value}";
+		}, $contentStyles, array_keys($contentStyles))) . ';' ?? '';
+	}
+
+	public static function innerStylesCalcString(array $style): string
+	{
+		$innerStyles = [];
+
+		if (empty($style['spacing']['blockGap'] ?? '')) {
+			return '';
+		}
+
+		if (strpos($style['spacing']['blockGap'], 'var:preset') !== false) {
+			$parts = explode('|', $style['spacing']['blockGap']);
+			$size = $parts[2] ?? '';
+			$innerStyles['--wp--style--block-gap'] = "var(--wp--preset--spacing--{$size})";
+		} else {
+			$innerStyles['--wp--style--block-gap'] = $style['spacing']['blockGap'];
+		}
+
+		if (empty($innerStyles)) {
+			return '';
+		}
+
+		return implode('; ', array_map(function ($value, $property) {
+			return "{$property}: {$value}";
+		}, $innerStyles, array_keys($innerStyles))) . ';' ?? '';
+	}
 }
--- src/Blocks/PageTeaserCover/render.php
+++ src/Blocks/PageTeaserCover/render.php
@@ -1,137 +1,74 @@
 <?php
 
-use PT\MustUse\Package\Media as MediaPackage;
+namespace PT\MustUse\Blocks\PageTeaserCover;
 
-if (is_singular('post') && get_page_template_slug() === 'single-no-thumbnail') {
+$postId = (int) ($attributes['postId'] ?? null);
+
+if (!$postId) {
 	return '';
 }
 
-$post_id = get_the_ID();
+
+
+
+$imageSize = $attributes['imageSize'];
+$title = esc_html(get_the_title($postId));
+$excerpt = get_the_excerpt($postId);
+$link = get_permalink($postId);
+$linkText = esc_html($attributes['linkText'] ?? $title);
 $classNameBase = wp_get_block_default_classname($block->name);
-$align = $attributes['align'] ?? '';
 
-$className = $attributes['className'] ?? '';
+$contentStyles = Block::contentStylesCalcString($attributes['style']);
 
-if (!empty($className)) {
-	$className = "{$className} ";
+if (!empty($contentStyles)) {
+	$contentStyles = 'style="' . $contentStyles . '"';
 }
 
-$media_size = 'medium';
-switch ($align) {
-	case 'wide':
-		$media_size = 'gutenberg_wide';
-		break;
-	case 'full':
-		$media_size = 'full';
-		break;
+$innerStyles = Block::innerStylesCalcString($attributes['style']);
+if (!empty($innerStyles)) {
+	$innerStyles = 'style="' . $innerStyles . '"';
 }
 
-if (!empty($attributes['resolution'])) {
-	$media_size = $attributes['resolution'];
-}
+$image = wp_get_attachment_image(get_post_thumbnail_id($postId), $imageSize, false, ['class' => "{$classNameBase}__image"]);
 
-$content = '';
-$media_package = new MediaPackage();
-
-if (!empty($video_url = get_post_meta($post_id, 'video_ref', true))) {
-
-	if (is_singular('post') || is_singular('page') && !$attributes['hideInlineEmbed'] ?? false) {
-
-		$url_parts = parse_url($video_url, PHP_URL_QUERY);
-
-		if (strpos($video_url, 'youtube.com') !== false || strpos($video_url, 'youtu.be') !== false) {
-			$video_url = add_query_arg('hq', '1', $video_url);
-		}
-
-		$video_player = wp_oembed_get($video_url);
-
-		if (empty($video_player)) {
-			return $video_player;
-		}
-
-		$video_player = $media_package->addHqParam($video_player);
-
-		$content = sprintf(
-			'<figure class="%1$s%2$s__figure %2$s__figure--video">%3$s</figure>',
-			$className,
-			$classNameBase,
-			$video_player,
-		);
-	} else {
-
-		$thumbnail = $media_package->getVideoThumbnail($video_url);
-
-		if (!empty($thumbnail)) {
-			$content = sprintf(
-				'<figure class="%1$s%2$s__figure %2$s__figure--%3$s"><a href="%6$s"><img class="%2$s__image" src="%4$s" alt="%5$s" /></a></figure>',
-				$className,
-				$classNameBase,
-				$media_size,
-				$media_package->getVideoThumbnail($video_url),
-				get_the_title($post_id),
-				get_the_permalink($post_id)
-			);
-		}
-	}
-} elseif (has_post_thumbnail($post_id)) {
-
-	$thumbnail_id = get_post_thumbnail_id($post_id);
-	$image = wp_get_attachment_image_url($thumbnail_id, $media_size);
-	$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-
-	if (!empty($image)) {
-
-		$image = sprintf(
-			'<img loading="lazy" decode="sync" class="%1$s__image" src="%2$s" alt="%3$s" />',
-			$classNameBase,
-			$image,
-			$alt
-		);
-
-		// Weirdness when placing this block in a post list on a page
-		// This ensures the correct link being set
-		if (is_singular(get_post_type(get_the_ID()))) {
-			$content = sprintf(
-				'<figure class="%1$s%2$s__figure %2$s__figure--%3$s">%4$s</figure>',
-				$className,
-				$classNameBase,
-				$media_size,
-				$image
-			);
-		} else {
-			$content = sprintf(
-				'<figure class="%1$s%2$s__figure %2$s__figure--%3$s"><a href="%5$s" title="%6$s">%4$s</a></figure>',
-				$className,
-				$classNameBase,
-				$media_size,
-				$image,
-				get_the_permalink($post_id),
-				'Article: ' . get_the_title($post_id)
-			);
-		}
-	}
-}
-
-if (!is_singular('post') && empty($content)) {
-	return sprintf(
-		'<div aria-hidden class="%1$s%2$s__figure %2$s__figure--empty"><a href="%3$s">%4$s</a></div>',
-		$className,
+if (!empty($image)) {
+	$image = sprintf(
+		'<figure class="%1$s__figure">%2$s</figure>',
 		$classNameBase,
-		get_the_permalink($post_id),
-		get_the_title($post_id)
+		$image
 	);
-}
-
-if (!empty($align)) {
-	$align = " align{$align}";
-}
-
-if (empty($content)) {
-	return;
+} else {
+	$image = sprintf(
+		'<figure class="%1$s__figure %1$s__figure--empty"></figure>',
+		$classNameBase
+	);
 }
 
 ?>
 
-<div class="<?php echo $className . ' ' . $classNameBase . $align; ?>">
-	<?php echo $content; ?>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<div class="<?php echo $classNameBase; ?>__inner" <?php echo $innerStyles; ?>>
+		<div class="<?php echo $classNameBase; ?>__content" <?php echo $contentStyles; ?>>
+			<h2 class="<?php echo $classNameBase; ?>__title">
+				<?php echo $title; ?>
+			</h2>
+
+			<?php if (!empty($excerpt)) { ?>
+				<div class="<?php echo $classNameBase; ?>__excerpt">
+					<?php echo $excerpt; ?>
+				</div>
+			<?php } ?>
+
+			<div class="<?php echo $classNameBase; ?>__link-wrapper wp-block-button is-style-with-arrow-right has-smaller-font-size">
+				<a href="<?php echo $link; ?>" class="<?php echo $classNameBase; ?>__link wp-block-button__link has-transparent-background-color has-background wp-element-button">
+					<span class="<?php echo $classNameBase; ?>__link-text"><?php echo $linkText; ?></span>
+				</a>
+			</div>
+		</div>
+
+		<?php echo $image; ?>
+
+		<a href=" <?php echo $link; ?>" class="<?php echo $classNameBase; ?>__link--flood"><?php echo $linkText; ?></a>
+
+	</div>
 </div>
