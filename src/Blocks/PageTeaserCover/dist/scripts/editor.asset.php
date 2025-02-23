21f223d40f81e120e1129d5a0f7478c87e02f6c1
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-02-23 13:30:02 +0100

    Compile

--- src/Blocks/PageTeaserCover/dist/scripts/editor.asset.php
+++ src/Blocks/PageTeaserCover/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '8deaf5d30f60b5654b5d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd3602849753291963292');
