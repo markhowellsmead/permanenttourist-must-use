5b715b37a90ea7976e014da9aa60d6a6de450ae5
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-07-13 14:14:44 +0200

    Catch JS error, tweak layout

--- assets/dist/scripts/attachment-from-ftp.asset.php
+++ assets/dist/scripts/attachment-from-ftp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '5e5ac23a165adc1e3ddc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '5c7b8ce85367cd268301');
