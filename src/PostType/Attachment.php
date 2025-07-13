4ef7020248b3000a2e22a7f9f412e0a83a4fe3dc
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-07-13 19:19:52 +0200

    Modify rest attachment query

--- src/PostType/Attachment.php
+++ src/PostType/Attachment.php
@@ -2,6 +2,7 @@
 
 namespace PT\MustUse\PostType;
 
+use WP_Query;
 use WP_REST_Attachments_Controller;
 use WP_REST_Request;
 
@@ -10,6 +11,7 @@ class Attachment
 	public function run()
 	{
 		add_action('init', [$this, 'registerRestFields']);
+		add_filter('rest_attachment_query', [$this, 'filterByPhotoPosts'], 10, 2);
 	}
 
 	public function registerRestFields()
@@ -43,5 +45,35 @@ class Attachment
 				return $fields;
 			}
 		]);
+	}
+
+	public function filterByPhotoPosts(array $args, WP_REST_Request $request)
+	{
+		if (!$request->get_param('pt_nophoto_posts')) {
+			return $args;
+		}
+
+		global $wpdb;
+
+		$used_ids = $wpdb->get_col("
+        SELECT meta_value
+        FROM {$wpdb->postmeta} m
+        INNER JOIN {$wpdb->posts} p ON m.post_id = p.ID
+        WHERE m.meta_key = '_thumbnail_id'
+        AND p.post_type = 'photo'
+        AND p.post_status != 'trash'");
+
+		if (empty($used_ids)) {
+			return $args;
+		}
+
+		$post__not_in = $args['post__not_in'] ?? [];
+
+		$args['post__not_in'] = array_merge($post__not_in, $used_ids);
+
+		// Remove the 'pt_nophoto_posts' parameter to avoid infinite recursion
+		unset($args['pt_nophoto_posts']);
+
+		return $args;
 	}
 }
