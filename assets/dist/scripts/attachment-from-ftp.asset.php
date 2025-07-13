d4caa0537bff07422044189872209cde5b8f6a6c
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-07-13 19:20:34 +0200

    Multiset loading

--- assets/dist/scripts/attachment-from-ftp.asset.php
+++ assets/dist/scripts/attachment-from-ftp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '5c7b8ce85367cd268301');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '34e125b31d21fc8a0a61');
--- assets/src/scripts/attachment-from-ftp/_api.js
+++ assets/src/scripts/attachment-from-ftp/_api.js
@@ -1,46 +1,111 @@
-// import apiFetch from '@wordpress/api-fetch';
 import { useEffect, useState } from '@wordpress/element';
 
-const apiGet = (apiUrl, nonce) => {
+export const apiStates = {
+    IDLE: 'IDLE',
+    LOADING: 'LOADING',
+    SUCCESS: 'SUCCESS',
+    ERROR: 'ERROR',
+};
+
+export const apiGet = (apiUrl, nonce, per_page) => {
+    const [perPage] = useState(per_page || 24);
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const doRequest = async () => {
+        const fetchAllPages = async () => {
             const headers = {
                 'Content-Type': 'application/json',
             };
-
             if (nonce) {
                 headers['X-WP-Nonce'] = nonce;
             }
 
-            try {
-                const response = await fetch(apiUrl, { headers });
-                const jsonData = await response.json();
+            let page = 1;
+            let allData = [];
+            let totalPages = 1;
 
-                if (!response.ok) {
-                    setError(jsonData);
-                } else {
-                    setData(jsonData);
-                }
-            } catch (error) {
-                setError(error);
+            try {
+                do {
+                    const pagedUrl = `${apiUrl}&page=${page}`;
+                    const response = await fetch(pagedUrl, { headers });
+                    const jsonData = await response.json();
+
+                    if (!response.ok) {
+                        throw jsonData;
+                    }
+
+                    allData = [...allData, ...jsonData];
+                    page++;
+                } while (page <= totalPages);
+
+                setData(allData);
+            } catch (err) {
+                setError(err);
             } finally {
                 setLoading(false);
             }
         };
 
-        doRequest();
-
-        return () => {
-            // Cleanup function
-            //console.log('Component unmounted! Cleanup performed.');
-        };
-    }, []);
+        totalPages = parseInt(response.headers.get('X-WP-TotalPages')) || 1;
+        fetchAllPages();
+    }, [apiUrl, nonce, perPage]);
 
     return { data, loading, error };
 };
 
-export { apiGet };
+export const apiGetAll = (apiUrl, nonce, per_page) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchAllPages = async () => {
+            const headers = {
+                'Content-Type': 'application/json',
+            };
+            if (nonce) {
+                headers['X-WP-Nonce'] = nonce;
+            }
+            let page = 1;
+            let allData = [];
+            let totalPages = 1;
+
+            try {
+                do {
+                    const pagedUrl = `${apiUrl}&per_page=100&page=${page}`;
+                    const response = await fetch(pagedUrl, { headers });
+                    const jsonData = await response.json();
+                    const responseHeaders = response.headers;
+
+                    if (responseHeaders.has('X-WP-TotalPages')) {
+                        console.log(
+                            'X-WP-TotalPages header found',
+                            responseHeaders.get('X-WP-TotalPages')
+                        );
+                    }
+
+                    if (!response.ok) {
+                        throw jsonData;
+                    }
+
+                    allData = [...allData, ...jsonData];
+                    totalPages = parseInt(response.headers.get('X-WP-TotalPages')) || 1;
+                    page++;
+                    console.log(`Loading page ${page} of ${totalPages}…`);
+                } while (page <= totalPages && page <= per_page / 100);
+
+                setData(allData);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchAllPages();
+    }, [apiUrl, nonce]);
+
+    return { data, loading, error };
+};
