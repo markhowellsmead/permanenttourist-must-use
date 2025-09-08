2fc2ae4a5151c41de2780d199255ce7edbf650d2
Mark Howells-Mead <mark@sayhello.ch>
2025-09-08 17:51:17 +0200

    Disable keywords field

--- assets/dist/scripts/attachment-from-ftp.asset.php
+++ assets/dist/scripts/attachment-from-ftp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '75fe78665fa370b10a89');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => 'edd1da1305b751e40533');
