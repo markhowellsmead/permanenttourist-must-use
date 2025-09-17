14bd03cd53dd4c651d89372287a7294e1d66fa3d
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-09-17 23:14:02 +0200

    Version,recompile

--- src/Blocks/PageTeaserCover/dist/scripts/editor.asset.php
+++ src/Blocks/PageTeaserCover/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd3602849753291963292');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'c2c3adbfbaad05b604f1');
