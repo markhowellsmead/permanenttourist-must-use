36a994816f34cec0a44d0d71dd6982a59e02a53e
Mark Howells-Mead <permanent.tourist@gmail.com>
2026-03-14 18:37:40 +0100

    Version

--- permanenttourist-must-use.php
+++ permanenttourist-must-use.php
@@ -7,7 +7,10 @@ GitHub Plugin URI: https://github.com/markhowellsmead/permanenttourist-must-use
 Primary Branch: main
 Description: This WordPress plugin contains all of the blocks and portable functionality for permanenttourist.ch.
 Author: Mark Howells-Mead
-Version: 0.37.0
+Version: 0.37.1
+Requires at least: 6.9
+Tested up to: 6.9
+Requires PHP: 8.3
 Author URI: https://www.permanenttourist.ch/
 Text Domain: pt-must-use
 Domain Path: languages
